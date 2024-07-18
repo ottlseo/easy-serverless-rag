0077ad750136e9fe27c226c40f92755bda0b1006
ottlseo <dotsi@ewhain.net>
2024-07-19 07:01:13 +0900

    Feat: update skeleton files

--- rag-for-complex-docs/lib/StepFunctionsStack/lambdas/generateSummarizationLambda/lambda_function.py
+++ rag-for-complex-docs/lib/StepFunctionsStack/lambdas/generateSummarizationLambda/lambda_function.py
@@ -43,7 +43,6 @@ summarize_chain = prompt | llm_text | StrOutputParser()
 
 img_info = [image_to_base64(img_path) for img_path in images if os.path.basename(img_path).startswith("figure")]
 
-@retry(total_try_cnt=5, sleep_in_sec=10, retryable_exceptions=(botocore.exceptions.EventStreamError))
 def summary_img(summarize_chain, img_base64):
 
     img = Image.open(BytesIO(base64.b64decode(img_base64)))
